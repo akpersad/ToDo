a750a9926aad263a45f28e68e596815fc28dd5bc
github username <github email address>
2016-02-21 16:15:23 -0500

    added universal ToDos

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,28 @@
 class ItemsController < ApplicationController
-	before_filter :authenticate_user!
+	# before_filter :authenticate_user!
 
 	def home
-		
+
 		if user_signed_in?
 			@items = Item.where(user_id: current_user.id)
 		else
-			@items = Item.all
+			@items = Item.where(user_id:0)
 		end
-		# @items = Item.all
+		# binding.pry
 	end
 
 	def create
+		
 		@item = Item.create(item_params)
-		@item.user_id = current_user.id
-		if @item.save
+		if user_signed_in?
+			@item.user_id = current_user.id
+			if @item.save
+			end
+		else
+			@item.user_id = 0
+			if@item.save
+			end
 		end
-		# binding.pry
 
 		respond_to do |format|
 		format.html {redirect_to root_path}
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,4 @@
 class Item < ActiveRecord::Base
-	validates :name, uniqueness:true
+	# validates :name, uniqueness:true
 	# :belongs_to => :user
 end
