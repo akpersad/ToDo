4aa3bcec67f3b7b76d8b1d9312de3eda27fe227b
github username <github email address>
2016-02-22 21:37:40 -0500

    Sort Start

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,9 @@ class ItemsController < ApplicationController
 	def home
 
 		if user_signed_in?
-			@items = Item.where(user_id: current_user.id)
+			@items = Item.where(user_id: current_user.id).order(sort_column + " " + sort_direction)
 		else
-			@items = Item.where(user_id:0)
+			@items = Item.where(user_id:0).order(sort_column + " " + sort_direction)
 		end
 	end
 
@@ -40,5 +40,15 @@ class ItemsController < ApplicationController
 
 	def item_params
 		params.require(:item).permit(:name)
+		# binding.pry
 	end
+	
+	def sort_column
+    	Item.column_names.include?(params[:sort]) ? params[:sort] : "created_at"
+    end
+  
+	def sort_direction
+	    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+	end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+	def sortable(column, title = nil)
+ 		title ||= column.titleize
+  		css_class = column == sort_column ? "current #{sort_direction}" : nil
+  		direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+		link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+	end
 end
+
+
+
+# <%if false%>
+# 	<%@items = @items.order("created_at desc")%>
+# <%else%>
+# 	<%@items = @items.order("created_at desc").reverse%>
+# <%end%>
